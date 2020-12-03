84c34e80e3217b425610a6fc74c86defe9100944
Muhammed Karakul <muhammed.karakul@loodos.com>
2020-12-03 15:13:53 +0300

    Xcode project created.

---
+++ Coordinator Example/Coordinator Example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Coordinator Example
+//
+//  Created by Muhammed Karakul on 3.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
