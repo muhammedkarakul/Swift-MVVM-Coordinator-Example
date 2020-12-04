45a660f5014f83f91816ef23472da4606ebfd259
Muhammed Karakul <muhammed.karakul@loodos.com>
2020-12-04 13:33:20 +0300

    SnapKit added to project via CocoaPods.

--- Coordinator Example/View/Initial/InitialViewController.swift
+++ Coordinator Example/View/Initial/InitialViewController.swift
@@ -12,6 +12,7 @@ class InitialViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .white
+        title = "Initial View"
     }
 }
 
